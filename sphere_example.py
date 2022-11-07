2a2d3c8b9a7e08887a39a84eed239d852b65121a
Ryan Nemiroff <ryguyn@gmail.com>
2022-11-07 00:03:49 -0800

    Update sphere example

--- sphere_example.py
+++ sphere_example.py
@@ -14,16 +14,13 @@ def plot_sphere(ax, x, y, z, r):
 
 m = GEKKO()
 nt = 51
-m.time = np.linspace(0, 5, nt)
-
-# useful vector for gekko equations
-final_m = np.zeros(nt)
-final_m[-1] = 1.0
-final = m.Param(value=final_m)
+m.time = np.linspace(0, 10, nt)
 
 a = [] # acceleration (control input)
 v = [] # velocity
 p = [] # position
+p_f = [] # final position
+v_f = [] # final velocity
 
 # 3D
 for _ in range(3):
@@ -36,14 +33,26 @@ for _ in range(3):
     m.Equation(p[-1].dt() == v[-1])
     m.Equation(v[-1].dt() == a[-1])
 
-    # final condition (reach position (10, 10, 10))
-    m.Equation((p[-1] - 10)*final == 0)
+    # set up end constraints
+    p_f.append(m.Var()) # should be FV instead of Var but doesn't work for some reason
+    m.Connection(p[-1], p_f[-1], 'end', 'end')
+    v_f.append(m.Var())
+    m.Connection(v[-1], v_f[-1], 'end', 'end')
 
-# spherical obstacle
-m.Equation((p[0]-5)**2 + (p[1]-5)**2 + (p[2]-5)**2 >= 1)
+# end constraints
+m.Equations((p_f[0] == 10, p_f[1] == 10, p_f[2] == 10))
+m.Equations(vf == 0 for vf in v_f)
+
+# spherical obstacles
+spheres = [
+    (1, 1, 1, 1), # x, y, z, r
+    (8, 8, 8, 2)
+]
+for s in spheres:
+    m.Equation((p[0]-s[0])**2 + (p[1]-s[1])**2 + (p[2]-s[2])**2 >= s[3]**2)
 
 # minimize control input
-m.Minimize((a[0]*a[0] + a[1]*a[1] + a[2]*a[2]))
+m.Minimize(a[0]**2 + a[1]**2 + a[2]**2)
 
 m.options.IMODE = 6 # control
 m.solve()
@@ -58,12 +67,14 @@ plt.subplot(2,1,1)
 plt.plot(m.time,a[0].value,'-',label='a_x')
 plt.plot(m.time,a[1].value,'-',label='a_y')
 plt.plot(m.time,a[2].value,'-',label='a_z')
+plt.grid()
 plt.legend(loc='best')
 plt.ylabel('Input')
 plt.subplot(2,1,2)
 plt.plot(m.time,p[0].value,'-',label='p_x')
 plt.plot(m.time,p[1].value,'-',label='p_y')
 plt.plot(m.time,p[2].value,'-',label='p_z')
+plt.grid()
 plt.ylabel('Output')
 plt.xlabel('Time')
 plt.legend(loc='best')
@@ -71,6 +82,7 @@ plt.legend(loc='best')
 plt.figure()
 ax = plt.axes(projection='3d')
 ax.plot3D(p[0].value, p[1].value, p[2].value)
-plot_sphere(ax, 5, 5, 5, 1)
+for s in spheres:
+    plot_sphere(ax, s[0], s[1], s[2], s[3])
 
 plt.show()

61acf64c1efbd4201b9aaa06f5007da62758e064
Aaron John Sabu <aaronjohnsabu1999@gmail.com>
2022-11-15 17:50:49 -0800

    Adding dynamics... (incomplete)

--- sphere_example.py
+++ sphere_example.py
@@ -12,36 +12,98 @@ def plot_sphere(ax, x, y, z, r):
     z = z + r * np.cos(v)
     ax.plot_surface(x, y, z, cmap=plt.cm.YlGnBu_r)
 
+def quat_mult(q_1, q_2):
+    w_1, x_1, y_1, z_1 = q_1
+    w_2, x_2, y_2, z_2 = q_2
+    w = w_1 * w_2 - x_1 * x_2 - y_1 * y_2 - z_1 * z_2
+    x = w_1 * x_2 + x_1 * w_2 + y_1 * z_2 - z_1 * y_2
+    y = w_1 * y_2 + y_1 * w_2 + z_1 * x_2 - x_1 * z_2
+    z = w_1 * z_2 + z_1 * w_2 + x_1 * y_2 - y_1 * x_2
+    return [w, x, y, z]
+
+def quat_conj(q):
+    w, x, y, z = q
+    return [w, -x, -y, -z]
+
+def quat_rot(q, v):
+    q_conj = quat_conj(q)
+    q_v = [0, v[0], v[1], v[2]]
+    q_v_rot = quat_mult(q, quat_mult(q_v, q_conj))
+    return [q_v_rot[1], q_v_rot[2], q_v_rot[3]]
+
 m = GEKKO()
 nt = 51
 m.time = np.linspace(0, 10, nt)
 
-a = [] # acceleration (control input)
-v = [] # velocity
-p = [] # position
-p_f = [] # final position
-v_f = [] # final velocity
+p = [] # linear position
+v = [] # linear velocity
+a = [] # linear acceleration
+q = [] # angular position
+w = [] # angular velocity
+
+F_I = [] # inertial forces
+F_B = [] # body forces
+M_B = [] # body moments
+
+p_f = [] # final linear position
+v_f = [] # final linear velocity
+q_f = [] # final angular position
+w_f = [] # final angular velocity
+
+M = 10
+J = np.diag([5, 3, 2])
 
 # 3D
 for _ in range(3):
-    # initialize varibales
+    # initialize variables
+    # a.append(m.Var(value=0))
     a.append(m.Var(value=None, lb=-1, ub=1, fixed_initial=False))
     v.append(m.Var(value=0))
     p.append(m.Var(value=0))
+    q.append(m.Var(value=0))
+    w.append(m.Var(value=0))
 
-    # dynamics
-    m.Equation(p[-1].dt() == v[-1])
-    m.Equation(v[-1].dt() == a[-1])
+    F_I.append(m.Var(value=0))
+    F_B.append(m.Var(value=0, fixed_initial=False))
+    M_B.append(m.Var(value=0, fixed_initial=False))
 
-    # set up end constraints
     p_f.append(m.Var()) # should be FV instead of Var but doesn't work for some reason
-    m.Connection(p[-1], p_f[-1], 'end', 'end')
     v_f.append(m.Var())
-    m.Connection(v[-1], v_f[-1], 'end', 'end')
+    q_f.append(m.Var())
+    w_f.append(m.Var())
+
+q.append(m.Var(value=0))
+q_f.append(m.Var(value=0))
+
+for i in range(3):
+    # dynamics
+    m.Equation(p[i].dt() == v[i])
+    m.Equation(v[i].dt() == a[i])
+    m.Equation(M*a[i] == F_I[i] + quat_mult(quat_mult(q, [0, F_B[0], F_B[1], F_B[2]]), quat_conj(q))[i])
+    m.Equation(q[i].dt() == 0.5 * quat_mult(q, [0, w[0], w[1], w[2]])[i])
+    m.Equation(np.sum(J[i][j] * w[j].dt() for j in range(len(w))) == - np.cross(w,
+                                                                                np.multiply(J,
+                                                                                            w))[i]
+                                                                        + M_B[i])
+    
+    # set up end constraints
+    m.Connection(p[i], p_f[i], 'end', 'end')
+    m.Connection(v[i], v_f[i], 'end', 'end')
+    m.Connection(q[i], q_f[i], 'end', 'end')
+    m.Connection(w[i], w_f[i], 'end', 'end')
 
 # end constraints
-m.Equations((p_f[0] == 10, p_f[1] == 10, p_f[2] == 10))
-m.Equations(vf == 0 for vf in v_f)
+P_F = [10, 10, 10]
+m.Equations(p_f[i] == P_F[i] for i in range(0, len(p_f)))
+m.Equations(v_f[i] == 0      for i in range(0, len(v_f)))
+m.Equations(q_f[i] == 0      for i in range(1, len(q_f)))
+m.Equations(w_f[i] == 0      for i in range(0, len(w_f)))
+
+m.Equation(F_I[0] ==       0)
+m.Equation(F_I[1] ==       0)
+m.Equation(F_I[2] == - M * 9.81)
+m.Equation(F_B[0] ==       0)
+m.Equation(F_B[1] ==       0)
 
 # spherical obstacles
 spheres = [
@@ -52,7 +114,7 @@ for s in spheres:
     m.Equation((p[0]-s[0])**2 + (p[1]-s[1])**2 + (p[2]-s[2])**2 >= s[3]**2)
 
 # minimize control input
-m.Minimize(a[0]**2 + a[1]**2 + a[2]**2)
+m.Minimize(F_B[2]**2 + M_B[2]**2)
 
 m.options.IMODE = 6 # control
 m.solve()
